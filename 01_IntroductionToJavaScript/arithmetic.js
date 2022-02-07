706d0cf89f1895a09bf6ba30e310a27ff6e9447b
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-07 22:38:51 +0900

    added an example

---
+++ 01_IntroductionToJavaScript/arithmetic.js
@@ -0,0 +1,28 @@
+// 2 to the power 53
+console.log(Math.pow(2, 52));
+
+console.log(Math.PI, Math.E);
+
+
+// Base 10 logarithm of 100
+console.log(Math.log(100) / Math.LN10);
+
+// Pseudo-random number x where 0 <=x < 1.0
+console.log(Math.random());
+
+// 3**0.5: the square root of 3
+console.log(Math.sqrt(3));
+
+
+// Return the largest argument
+console.log(Math.max(1, 2, 3));
+
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Infinity);
+console.log(0/0);
+console.log(-1/Infinity)
+
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_SAFE_INTEGER)
