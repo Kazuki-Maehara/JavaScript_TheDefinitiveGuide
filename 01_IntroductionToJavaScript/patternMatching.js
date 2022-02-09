1a1bd3f12d26a7ee57d959715ed4801bd5cc6099
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-09 19:22:13 +0900

    added an example

---
+++ 01_IntroductionToJavaScript/patternMatching.js
@@ -0,0 +1,8 @@
+let text = "testing: 1, 2, 3";
+let pattern = /\d+/g;
+
+console.log(pattern.test(text));
+console.log(text.search(pattern));
+console.log(text.match(pattern));
+console.log(text.replace(pattern, "#"));
+console.log(text.split(/\D+/));
