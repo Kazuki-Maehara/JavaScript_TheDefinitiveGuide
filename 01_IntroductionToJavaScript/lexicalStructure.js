4f04ac192db92a171d4391fc4c2068af56691e67
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-15 21:27:34 +0900

    added an example

---
+++ 01_IntroductionToJavaScript/lexicalStructure.js
@@ -0,0 +1,26 @@
+// This is a single-line comment.
+
+
+/* This is also a comment */  // and there is another comment.
+
+
+/*
+* This is a multi-line comment. The extra * characters at the start of
+* each line are not a required part of the syntax; they just look cool!
+*/
+
+let i;
+let my_variable_name;
+let v13;
+let _dummy;
+let $str
+
+let café = 1;
+console.log('cafe\u00e9');
+console.log('cafe\u{E9}');
+
+let a
+a
+=
+3
+console.log(a);
