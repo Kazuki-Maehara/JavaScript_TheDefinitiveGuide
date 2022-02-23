2987bec9e9c4b51b643e08b1da2542397e708252
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-23 18:17:04 +0900

    added an example

---
+++ 04_ExpressionsAndOperators/globalEval.js
@@ -0,0 +1,17 @@
+const geval = eval;
+let x = "global", y = "global";
+
+function f() {
+  let x = "local";
+  eval("x += 'changed';");
+  return x;
+}
+
+function g() {
+  let y = "local";
+  geval("y += 'changed';");
+  return y;
+}
+
+console.log(f(), x);
+console.logo(g(), y);
