013c86778f3a0466f352ba79a742d40cbb6a8462
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-24 21:30:51 +0900

    added an example

---
+++ 04_ExpressionsAndOperators/inOperator.js
@@ -0,0 +1,12 @@
+let point = {x: 1, y:1};
+
+console.log("x" in point);
+console.log("z" in point);
+console.log("toString" in point);
+
+
+let data = [7, 8, 9];
+
+console.log("0" in data);;
+console.log(1 in data);
+console.log(3 in data);
