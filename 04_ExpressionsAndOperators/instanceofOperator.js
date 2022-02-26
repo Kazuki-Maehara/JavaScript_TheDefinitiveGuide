e0b049daee538a1f8b902798fb914d928b3ee057
Kazuki Maehara <treize.citron13@gmail.com>
2022-02-26 11:16:34 +0900

    added an example

---
+++ 04_ExpressionsAndOperators/instanceofOperator.js
@@ -0,0 +1,11 @@
+let d = new Date();
+
+console.log(d instanceof Date);
+console.log(d instanceof Date);
+console.log(d instanceof Number);
+
+let a = [1, 2, 3];
+
+console.log(a instanceof Array);
+console.log(a instanceof Object);
+console.log(a instanceof RegExp);
