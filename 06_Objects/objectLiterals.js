cc9d29b3298a6a23ea0bf12d1ed371b7d479c728
Kazuki Maehara <treize.citron13@gmail.com>
2022-03-07 21:38:09 +0900

    added an example

---
+++ 06_Objects/objectLiterals.js
@@ -0,0 +1,17 @@
+let empty = {};
+let point = { x: 0, y: 0};
+let p2 = { x: point.x, y: point.y+1};
+let book = {
+  "main title": "JavaScript",
+  "sub-title": "The Definitive Guide",
+  for: "all audiences",
+  author: {
+    firstname: "David",
+    surname: "Flanagan"
+  },
+};
+
+console.log(empty);
+console.log(point);
+console.log(p2);
+console.log(book);
