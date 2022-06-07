3a2c2d060583eda503f5ade89e7a9962000db4de
Kazuki Maehara <treize.citron13@gmail.com>
2022-06-07 19:13:59 +0900

    added an example

---
+++ 07_Arrays/multidimensionalArrays.js
@@ -0,0 +1,19 @@
+// Create a multidimensional array.
+let table = new Array(10);
+
+// 10 rows of the table.
+for (let i = 0; i < table.length; i++) {
+  // Each row has 10 columns.
+  table[i] = new Array(10);
+}
+
+// Initialize the array.
+for (let row = 0; row < table.length; row++) {
+  for (let col = 0; col < table[row].length; col++) {
+    table[row][col] = row * col;
+  }
+}
+
+// Use the multidimensional array to compute 5*7.
+console.log(table[5][7]);
+console.log(table[9][9]);
