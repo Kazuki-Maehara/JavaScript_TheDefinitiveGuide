b34e0af105b3bde1472fdcb55337f44bbd89a502
Kazuki Maehara <treize.citron13@gmail.com>
2022-06-13 21:29:31 +0900

    added an example

---
+++ 08_Functions/arrowFunctions.js
@@ -0,0 +1,7 @@
+// Make a copy of an array with null elemtns removed.
+let filtered = [1, null, 2, 3, ].filter(x => x !== null);
+console.log(filtered);
+
+// Square some numbers.
+let squares = [1, 2, 3, 4, ].map(x => x * x);
+console.log(squares);
